b01418f4b646c985596c7f5db8e5008076d97129
Kamran Dar <Kamran.Dar@lumentum.com>
2022-11-26 23:27:57 -0500

    changes to intersection.c

--- sample_Intersection.c
+++ sample_Intersection.c
@@ -7,6 +7,7 @@
 #include <stdlib.h>
 #include <sys/iofunc.h>
 #include <sys/dispatch.h>
+#include <string.h>
 
 #include "constants.h"
 
@@ -31,49 +32,69 @@ void *grabber(void *);
 int main(int argc, char *argv[]){
 
     int run_duration = 120;
-    pthread_t threadID;
+    pthread_t thdID0, thdID1, thdID2;
 
     printf("Running sample intersection for %d seconds\n", run_duration);
 
     // Create the grabber thread.
-    pthread_create(&threadID, NULL, grabber, NULL);
+    if((pthread_create(&thdID0, NULL, grabber, NULL)) != 0){
+    	printf("Error in grabber thread\n");
+    	exit(EXIT_FAILURE);
+    };
     // Set its priority to 2 so that it's higher than the direction threads.
-    pthread_setschedprio(threadID, 2);
+    pthread_setschedprio(thdID0, 2);
 
     // Create the east_west thread.
-    pthread_create(&threadID, NULL, east_west, NULL);
-    // Set its priority to 1 so that it's lower than the grabber thread.
-    pthread_setschedprio(threadID, 1);
+    if((pthread_create(&thdID1, NULL, east_west, NULL)) != 0){
+    	printf("Error in east-west thread\n");
+    	exit(EXIT_FAILURE);
+    };    // Set its priority to 1 so that it's lower than the grabber thread.
+    pthread_setschedprio(thdID1, 1);
 
     // Create the north_south thread.
-    pthread_create(&threadID, NULL, north_south, NULL);
-    // Set its priority to 1 so that it's lower than the grabber thread.
-    pthread_setschedprio(threadID, 1);
-
+    if((pthread_create(&thdID2, NULL, north_south, NULL)) != 0){
+    	printf("Error in north-south thread\n");
+    	exit(EXIT_FAILURE);
+    };    // Set its priority to 1 so that it's lower than the grabber thread.
+    pthread_setschedprio(thdID2, 1);
 
     printf("Threads have all been generated\n");
 
     // Let program run for run_duration seconds.
     sleep(run_duration);
-
     return 0;
 }
 
 int get_duration(int priority)
 {
-	printf("Priority received is: %d\n", priority);
-	return 20 / priority;
+	if(priority <1){
+		printf("get_duration priority is 0 or -'ve\n");
+		exit(EXIT_FAILURE);
+	}else{
+		printf("Priority received is: %d\n", priority);
+	}
+	return (20 / priority);
 }
 
 void *east_west(void *arg)
 {
     while(1)
     {
-        pthread_mutex_lock(&mutex);
+    	int ret_code;
         duration = get_duration(priority);
-        printf("East to west is now green for %d seconds!\n", duration);
-        sleep(duration);
-        pthread_mutex_unlock(&mutex);
+
+    	ret_code = pthread_mutex_lock(&mutex);
+    	if(ret_code == EOK){
+            printf("East to west is now green for %d seconds!\n", duration);
+            sleep(duration);
+            if(ret_code != EOK){
+                if(ret_code != EOK){
+                	printf("pthread_mutex_unlock() failed %s\n", strerror(ret_code));
+                }
+            }else{
+            	printf("pthread_mutex_unlock() failed %s\n", strerror(ret_code));
+            }
+    	}
     }
 }
 
@@ -81,45 +102,78 @@ void *north_south(void *arg)
 {
     while(1)
     {
-        pthread_mutex_lock(&mutex);
+    	int ret_code;
         duration = get_duration(priority);
-        printf("North to south is now green for %d seconds!\n", duration);
-        sleep(duration);
-        pthread_mutex_unlock(&mutex);
+
+        ret_code = pthread_mutex_lock(&mutex);
+        if(ret_code == EOK){
+            printf("North to south is now green for %d seconds!\n", duration);
+            sleep(duration);
+            ret_code = pthread_mutex_unlock(&mutex);
+            if(ret_code != EOK){
+            	printf("pthread_mutex_unlock() failed %s\n", strerror(ret_code));
+            }
+        }else{
+        	printf("pthread_mutex_unlock() failed %s\n", strerror(ret_code));
+        }
     }
 }
 
-void *grabber(void *arg)
-{
-	pthread_mutex_lock(&mutex);
-	// Aquire connection id from the server's name.
-	int coid = name_open(CTRL_SERVER_NAME, 0);
+void *grabber(void *arg){
 
-	get_prio_msg_t prio_msg = {.type = GET_PRIO_MSG_TYPE};
-	traffic_count_msg_t traffic_msg = {.type = TRAFFIC_COUNT_MSG_TYPE, .count = 0};
-	get_prio_resp_t prio_resp;
-//	unsigned test_priority;
+	int ret_code, coid;
+	ret_code = pthread_mutex_lock(&mutex);
 
-	// Send request to block controller asking for a priority.
-	MsgSend(coid, &prio_msg, sizeof(prio_msg), &prio_resp, sizeof(prio_resp));
+	if(ret_code == EOK){
+		// Acquire connection id from the server's name.
+		coid = name_open(CTRL_SERVER_NAME, 0);
 
-	// Set the priority to the value obtained from the block controller.
-	printf("Received priority %u from server\n", prio_resp.priority);
-	priority = prio_resp.priority;
-	pthread_mutex_unlock(&mutex);
+		get_prio_msg_t prio_msg = {.type = GET_PRIO_MSG_TYPE};
+		traffic_count_msg_t traffic_msg = {.type = TRAFFIC_COUNT_MSG_TYPE, .count = 0};
+		get_prio_resp_t prio_resp;
+		//	unsigned test_priority;
 
-    while(1){
-        pthread_mutex_lock(&mutex);
-        // Obtain traffic count info somehow...
-        // For now, set to arbitary value
-        traffic_msg.count = 8;
+		// Send request to block controller asking for a priority.
+		MsgSend(coid, &prio_msg, sizeof(prio_msg), &prio_resp, sizeof(prio_resp));
 
-        // Send traffic count to block controller and receive back a priority.
-        MsgSend(coid, &traffic_msg, sizeof(traffic_msg), &prio_resp, sizeof(prio_resp));
+		// Set the priority to the value obtained from the block controller.
+		printf("Received priority %u from server\n", prio_resp.priority);
+		priority = prio_resp.priority;
+		ret_code = pthread_mutex_unlock(&mutex);
 
-        // Set the priority to the value obtained from the block controller.
-        priority = prio_resp.priority;
-        printf("Priority is now: %d\n", priority);
-        pthread_mutex_unlock(&mutex);
-    }
+        if(ret_code != EOK){
+        	printf("pthread_mutex_unlock() failed %s\n", strerror(ret_code));
+        }
+        else{
+        	printf("pthread_mutex_unlock() failed %s\n", strerror(ret_code));
+        }
+		while(1){
+
+			int ret_code;
+			ret_code = pthread_mutex_lock(&mutex);
+			if(ret_code == EOK){
+				// Obtain traffic count info somehow...
+				// For now, set to arbitary value
+				traffic_msg.count = 8;
+
+				// Send traffic count to block controller and receive back a priority.
+				MsgSend(coid, &traffic_msg, sizeof(traffic_msg), &prio_resp, sizeof(prio_resp));
+
+				// Set the priority to the value obtained from the block controller.
+				priority = prio_resp.priority;
+				printf("Priority is now: %d\n", priority);
+				ret_code = pthread_mutex_unlock(&mutex);
+				if(ret_code != EOK){
+					printf("pthread_mutex_unlock() failed %s\n", strerror(ret_code));
+				}else{
+					printf("pthread_mutex_unlock() failed %s\n", strerror(ret_code));
+				}
+			}
+			else{
+				printf("pthread_mutex_unlock() failed %s\n", strerror(ret_code));
+			}
+		}
+	}else{
+		printf("pthread_mutex_unlock() failed %s\n", strerror(ret_code));
+	}
 }
